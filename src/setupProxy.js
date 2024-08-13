41755f9ec9bb3135b1ad23c5244c584e736f7d51
David Torrero <david.torrero@gmail.com>
2024-08-13 18:30:10 +0100

    Add env for backend

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const { createProxyMiddleware } = require('http-proxy-middleware');
-
+const backend = process.env.BACKEND;
 module.exports = function(app) {
   // Proxy for /local
   app.use(
     '/local', // Match any route that starts with /local
     createProxyMiddleware({
-      target: 'http://localhost:3001', // The target server
+      target: `http://${backend}:3001`, // The target server
       changeOrigin: true,
       pathRewrite: (path, req) => {
         // Remove the /local prefix and return the rest of the path
